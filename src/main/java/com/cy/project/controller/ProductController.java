3e3d14369d5a6480b258514a80e24338b4e8fc43
sch <sungchaohui@gmail.com>
2021-03-06 05:47:51 +0800

    8!

--- src/main/java/com/cy/project/controller/ProductController.java
+++ src/main/java/com/cy/project/controller/ProductController.java
@@ -3,13 +3,12 @@ package com.cy.project.controller;
 
 import com.cy.project.entity.Product;
 import com.cy.project.service.ProductService;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Transactional
@@ -18,46 +17,95 @@ public class ProductController {
     private final ProductService ps;
     public ProductController(ProductService productService){this.ps=productService;}
 
-    @GetMapping(value = {"/","all"})
-    public String productAllPage(){
-
-        Page<Product> page = ps.findBySortPage("DESC", "productPrice", 2);
-
-        System.out.println("本頁頁碼:"+page.getNumber());
-        System.out.println("本頁筆數:"+page.getNumberOfElements());
-        System.out.println("每頁筆數:"+page.getSize());
-        System.out.println("全部筆數:"+page.getTotalElements());
-        System.out.println("全部頁數:"+page.getTotalPages());
-        System.out.println(page.get());
-        System.out.println(page.getPageable());
-        System.out.println(page.getSort());
-        for (Product p :page) {
-            System.out.println(p);
-        }
-
+    @GetMapping(value = "/")
+    public String index(){
         return "shop-grid";
     }
 
-    @GetMapping(value = "/category")
-    public String productCategoryPage(){
 
-        Page<Product> page = ps.findByCategorySortPage("七龍珠","","productPrice", 1);
+    @GetMapping(value ="/grid/all")
+    @ResponseBody
+    public Page<Product> productAllPage(){
 
-
-        System.out.println("本頁頁碼:"+page.getNumber());
-        System.out.println("本頁筆數:"+page.getNumberOfElements());
-        System.out.println("每頁筆數:"+page.getSize());
-        System.out.println("全部筆數:"+page.getTotalElements());
-        System.out.println("全部頁數:"+page.getTotalPages());
-        System.out.println(page.get());
-        System.out.println(page.getPageable());
-        System.out.println(page.getSort());
-
+        Page<Product> page = ps.findBySortPage("ASC", "productId", 0);
 
         for (Product p :page) {
             System.out.println(p);
         }
 
-        return "shop-grid";
+        return page;
+    }
+
+    @GetMapping(value ="/grid/{productType}/{sort}")
+    @ResponseBody
+    public Page<Product> productSort(@PathVariable String productType,@PathVariable String sort){
+
+        String sortType="ASC";
+        String sortBy="productId";
+        if("PriceDESC".equals(sort)){
+            sortType="DESC";
+            sortBy="productPrice";
+
+        }else if ("PriceASC".equals(sort)){
+            sortType="ASC";
+            sortBy="productPrice";
+        }
+
+        Page<Product> page;
+        if("all".equals(productType)){
+            page = ps.findBySortPage(sortType, sortBy, 0);
+        }else {
+            page = ps.findByCategorySortPage(productType,sortType,sortBy, 0);
+        }
+
+        for (Product p :page) {
+            System.out.println(p);
+        }
+
+        return page;
+    }
+
+    @GetMapping(value ="/grid/{productType}/{sort}/{pageIndex}")
+    @ResponseBody
+    public Page<Product> productPage(@PathVariable String productType,@PathVariable String sort,@PathVariable Integer pageIndex){
+
+        String sortType="ASC";
+        String sortBy="productId";
+        if("PriceDESC".equals(sort)){
+            sortType="DESC";
+            sortBy="productPrice";
+
+        }else if ("PriceASC".equals(sort)){
+            sortType="ASC";
+            sortBy="productPrice";
+        }
+
+        Page<Product> page;
+        if("all".equals(productType)){
+            page = ps.findBySortPage(sortType, sortBy, pageIndex-1);
+        }else {
+            page = ps.findByCategorySortPage(productType,sortType,sortBy, pageIndex-1);
+        }
+
+        for (Product p :page) {
+            System.out.println(p);
+        }
+
+        return page;
+    }
+
+
+
+    @GetMapping(value = "/grid/category/{category}")
+    @ResponseBody
+    public Page<Product> productCategoryPage(@PathVariable String category){
+
+        Page<Product> page = ps.findByCategorySortPage(category,"","productId", 0);
+
+        for (Product p :page) {
+            System.out.println(p);
+        }
+
+        return page;
     }
 }
