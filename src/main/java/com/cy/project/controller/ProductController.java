e50fd2e2d9bb68e1814a4caf293c08dfa72ff055
sch <sungchaohui@gmail.com>
2021-03-06 23:47:35 +0800

    9!

--- src/main/java/com/cy/project/controller/ProductController.java
+++ src/main/java/com/cy/project/controller/ProductController.java
@@ -4,10 +4,13 @@ package com.cy.project.controller;
 import com.cy.project.entity.Product;
 import com.cy.project.service.ProductService;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,97 +18,79 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ProductController {
 
     private final ProductService ps;
-    public ProductController(ProductService productService){this.ps=productService;}
+
+    public ProductController(ProductService productService) {
+        this.ps = productService;
+    }
 
     @GetMapping(value = "/")
-    public String index(){
+    public String index() {
         return "shop-grid";
     }
 
-
-    @GetMapping(value ="/grid/all")
+    @GetMapping(value = "/grid/{productType}/{sort}/{pageIndex}")
     @ResponseBody
-    public Page<Product> productAllPage(){
+    public Page<Product> productPage(@PathVariable String productType, @PathVariable String sort, @PathVariable Integer pageIndex) {
 
-        Page<Product> page = ps.findBySortPage("ASC", "productId", 0);
+        String sortType = "ASC";
+        String sortBy = "productId";
+        if ("PriceDESC".equals(sort)) {
+            sortType = "DESC";
+            sortBy = "productPrice";
 
-        for (Product p :page) {
-            System.out.println(p);
-        }
-
-        return page;
-    }
-
-    @GetMapping(value ="/grid/{productType}/{sort}")
-    @ResponseBody
-    public Page<Product> productSort(@PathVariable String productType,@PathVariable String sort){
-
-        String sortType="ASC";
-        String sortBy="productId";
-        if("PriceDESC".equals(sort)){
-            sortType="DESC";
-            sortBy="productPrice";
-
-        }else if ("PriceASC".equals(sort)){
-            sortType="ASC";
-            sortBy="productPrice";
+        } else if ("PriceASC".equals(sort)) {
+            sortType = "ASC";
+            sortBy = "productPrice";
         }
 
         Page<Product> page;
-        if("all".equals(productType)){
-            page = ps.findBySortPage(sortType, sortBy, 0);
-        }else {
-            page = ps.findByCategorySortPage(productType,sortType,sortBy, 0);
+        if ("all".equals(productType)) {
+            page = ps.findBySortPage(sortType, sortBy, pageIndex - 1);
+        } else {
+            page = ps.findByCategorySortPage(productType, sortType, sortBy, pageIndex - 1);
         }
 
-        for (Product p :page) {
+        for (Product p : page) {
             System.out.println(p);
         }
+        System.out.println("都走這裡");
 
         return page;
     }
 
-    @GetMapping(value ="/grid/{productType}/{sort}/{pageIndex}")
+    @GetMapping(value = "/keys/{sort}/{pageIndex}")
     @ResponseBody
-    public Page<Product> productPage(@PathVariable String productType,@PathVariable String sort,@PathVariable Integer pageIndex){
+    public Page<Product>  productKeysPage(@PathVariable String sort, @PathVariable Integer pageIndex, @RequestAttribute String[] arr) {
 
-        String sortType="ASC";
-        String sortBy="productId";
-        if("PriceDESC".equals(sort)){
-            sortType="DESC";
-            sortBy="productPrice";
 
-        }else if ("PriceASC".equals(sort)){
-            sortType="ASC";
-            sortBy="productPrice";
+        System.out.println(arr);
+
+        String sortType = "ASC";
+        String sortBy = "productId";
+        if ("PriceDESC".equals(sort)) {
+            sortType = "DESC";
+            sortBy = "productPrice";
+
+        } else if ("PriceASC".equals(sort)) {
+            sortType = "ASC";
+            sortBy = "productPrice";
         }
 
         Page<Product> page;
-        if("all".equals(productType)){
-            page = ps.findBySortPage(sortType, sortBy, pageIndex-1);
-        }else {
-            page = ps.findByCategorySortPage(productType,sortType,sortBy, pageIndex-1);
-        }
+        page = ps.findByKeywordsSortPage(arr, sortType, sortBy, pageIndex-1);
 
-        for (Product p :page) {
+
+        for (Product p : page) {
             System.out.println(p);
         }
+        System.out.println("??");
 
         return page;
     }
 
 
-
-    @GetMapping(value = "/grid/category/{category}")
-    @ResponseBody
-    public Page<Product> productCategoryPage(@PathVariable String category){
-
-        Page<Product> page = ps.findByCategorySortPage(category,"","productId", 0);
-
-        for (Product p :page) {
-            System.out.println(p);
-        }
-
-        return page;
+    @GetMapping(value = "/getError")
+    public String error(@PathVariable String category) {
+        return "不存在";
     }
 }
