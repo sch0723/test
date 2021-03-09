e84294dafc8316cb9ac2ec21b5ba10f01d0fd4de
sch <sungchaohui@gmail.com>
2021-03-09 17:12:25 +0800

    ??

--- src/main/java/com/cy/project/controller/UserController.java
+++ src/main/java/com/cy/project/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.cy.project.controller;
 
+import com.cy.project.entity.Users;
 import com.cy.project.service.UsersService;
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -25,22 +24,32 @@ public class UserController {
         return "login";
     }
 
+    @GetMapping(value = "/logout")
+    public String toLogout(HttpSession session){
+        session.removeAttribute("users");
+        return "login";
+    }
+
+
     @PostMapping (value = "/verifyUsers")
-    public String productById(@RequestParam(value="usersAccount")String usersAccount, @RequestParam(value="usersPwd")String usersPwd, Model model) {
+    public String productById(@RequestParam(value="usersAccount")String usersAccount, @RequestParam(value="usersPwd")String usersPwd,
+                              Model model, HttpSession session) {
 
         System.out.println(usersAccount);
         System.out.println(usersPwd);
 
-        if(us.findByUsersAccount(usersAccount)==null){
+        Users users = us.findByUsersAccount(usersAccount);
+        if(users==null){
             model.addAttribute("msg","帳號不存在");
             return "login";
         }
-        if(!us.findByUsersAccount(usersAccount).getUsersPwd().equals(usersPwd)){
+        if(!users.getUsersPwd().equals(usersPwd)){
             model.addAttribute("msg","密碼錯誤");
             return "login";
         }
-        model.addAttribute("msg","ss");
+        session.setAttribute("users",users);
 
+        System.out.println("111");
         return "redirect:/";
     }
 
